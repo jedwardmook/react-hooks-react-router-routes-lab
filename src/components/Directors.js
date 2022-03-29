962d6c89c93175aacea2bff0431fbecea2f89761
JedwardMook <jedwardmook@yahoo.com>
2022-03-29 13:39:30 -0500

    commit tree bed7b4787d099da8f073a091d34ddaa5530901be

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+const directorsPage = directors.map((director) => {
+  return (
+    <div key={director.name}>
+      <h3>Name: {director.name}</h3>
+      <p>Movies:</p>
+        <li>{director.movies[0]}</li>
+        <li>{director.movies[1]}</li>
+        <li>{director.movies[2]}</li>
+    </div>
+    )
+  })
+
+  return <div>
+            <h1>Directors Page</h1>
+            {directorsPage}
+        </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie) => {
+        return <div key={movie.title}>
+          <h3>{movie.title}</h3>
+          <p>Run time: {movie.time}mins</p>
+          {movie.genres.map((genre) => {
+                return <li key={genre}>{genre}</li>
+                    })}
+        </div>
+    })
+    }
+    </div>;
 }
 
 export default Movies;
